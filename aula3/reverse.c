4dc7515bcf8da986194ddf4f323c655a36e98295
Gustavo Santos <gefgu@hotmail.com>
2023-08-28 19:03:43 -0300

    aula3: reverse

--- aula3/reverse.c
+++ aula3/reverse.c
@@ -2,19 +2,31 @@
 #include "queue.h"
 
 /* */
-void reverse (Queue *q) {
-  /*Terminar*/	
+void reverse(Queue *q)
+{
+  Stack *s = create(getsize_queue(q));
+
+  while (!empty_queue(q))
+  {
+    push(s, dequeue(q));
+  }
+
+  while (!empty(s))
+  {
+    enqueue(q, pop(s));
+  }
 }
 
 /* */
-int main () {
+int main()
+{
   int i, n = 10;
-  Queue *q = create_queue (n+1);
-  for (i = 0; i < n; i++) 
-    enqueue (q, i);
-  print_queue(q);  
-  reverse (q);
-  print_queue(q);  
-  destroy_queue (q);
+  Queue *q = create_queue(n + 1);
+  for (i = 0; i < n; i++)
+    enqueue(q, i);
+  print_queue(q);
+  reverse(q);
+  print_queue(q);
+  destroy_queue(q);
   return 0;
 }
