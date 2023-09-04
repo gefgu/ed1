7d95b527f202d26ae3d3a58beac078b87bdde5a6
Gustavo Santos <gefgu@hotmail.com>
2023-09-04 18:52:33 -0300

    aula4: merge

--- aula4/merge.c
+++ aula4/merge.c
@@ -1,37 +1,55 @@
 #include "list.h"
 
 /* */
-List* merge (List *A, List *B){
-  /*Terminar*/	
-  List * a = A;
-  List * b = B;
-  List *merged = create();
-  while(a != NULL || b != NULL) {
-    if(a == NULL || b->data <= a->data) {
-      merged = insert_back(merged, b->data);
-      b = b->next;
-    } else if(b == NULL || a->data < b->data) {
-      merged = insert_back(merged, a->data);
-      a = a->next;
+List *merge(List *A, List *B)
+{
+  /*Terminar*/
+  List *start = NULL;
+  List *t = NULL;
+  if (A->data >= B->data)
+  {
+    start = B;
+    B = B->next;
+  }
+  else
+  {
+    start = A;
+    A = A->next;
+  }
+  t = start;
+
+  while (A != NULL || B != NULL)
+  {
+    if (A == NULL || B->data <= A->data)
+    {
+      t->next = B;
+      B = B->next;
+      t = t->next;
+    }
+    else if (B == NULL || A->data < B->data)
+    {
+      t->next = A;
+      A = A->next;
+      t = t->next;
     }
   }
-  return merged;
+
+  return start;
 }
 
 /* */
-int main () {
+int main()
+{
   int k;
   List *A = NULL, *B = NULL;
-  for (k = 10; k >= 0; k -= 2) 
-    A = insert (A, k);
-  for (k = 19; k >= 0; k -= 2) 
-    B = insert (B, k);
-  print (A);
-  print (B);
-  List *C = merge (A, B);
-  print (C);
-  destroy(A);
-  destroy(B);
-  destroy (C);
+  for (k = 10; k >= 0; k -= 2)
+    A = insert(A, k);
+  for (k = 19; k >= 0; k -= 2)
+    B = insert(B, k);
+  print(A);
+  print(B);
+  List *C = merge(A, B);
+  print(C);
+  destroy(C);
   return 0;
 }
