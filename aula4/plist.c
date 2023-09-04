f5efe721d7b1a6d363658b054cd4b2653fa913f5
Gustavo Santos <gefgu@hotmail.com>
2023-09-04 17:31:15 -0300

    aula4: poly-sum

--- aula4/plist.c
+++ aula4/plist.c
@@ -1,12 +1,14 @@
 #include "plist.h"
 
 /* */
-List* create () {
-  return NULL;	
+List *create()
+{
+  return NULL;
 }
 
 /* */
-List* insert (List *l, poly p) {
+List *insert(List *l, poly p)
+{
   List *node = (List *)malloc(sizeof(List));
   node->p = p;
   node->next = l;
@@ -14,23 +16,31 @@ List* insert (List *l, poly p) {
 }
 
 /* */
-void print (List *l) {
+void print(List *l)
+{
   List *t;
-  printf ("Poly: ");
-  for (t = l; t != NULL; t = t->next) {
-    if (t->p.c != 0) {	  
-      printf ("%d^%d", t->p.c, t->p.d);
-      if ((t->next != NULL) && (t->next->p.c > 0)) { printf ("+"); }
-    }  
-  }  
+  printf("Poly: ");
+  for (t = l; t != NULL; t = t->next)
+  {
+    if (t->p.c != 0)
+    {
+      printf("%d^%d ", t->p.c, t->p.d);
+      if ((t->next != NULL) && (t->next->p.c > 0))
+      {
+        printf(" + ");
+      }
+    }
+  }
   printf("\n");
 }
 
 /* */
-void destroy (List *l) {
-  if (l != NULL) {	
-    List *t = l->next; 
-    free (l); 
-    l = t;    
-  }  
+void destroy(List *l)
+{
+  if (l != NULL)
+  {
+    List *t = l->next;
+    free(l);
+    l = t;
+  }
 }
--- aula4/poly-sum.c
+++ aula4/poly-sum.c
@@ -1,29 +1,60 @@
 #include "plist.h"
 
-List* poly_sum (List *pA, List *pB) {
-  /*Terminar!*/	
+List *poly_sum(List *pA, List *pB)
+{
+  /*Terminar!*/
+  List *sum = create();
+  List *poly_ordered = create();
+  while (pA != NULL || pB != NULL)
+  {
+    if (pA->p.d > pB->p.d)
+    {
+      sum = insert(sum, (poly){pA->p.c, pA->p.d});
+      pA = pA->next;
+    }
+    else if (pB->p.d > pA->p.d)
+    {
+      sum = insert(sum, (poly){pB->p.c, pB->p.d});
+      pB = pB->next;
+    }
+    else
+    { // equal
+      sum = insert(sum, (poly){pA->p.c + pB->p.c, pA->p.d});
+      pA = pA->next;
+      pB = pB->next;
+    }
+  }
+
+  while (sum != NULL)
+  {
+    poly_ordered = insert(poly_ordered, sum->p);
+    sum = sum->next;
+  }
+
+  return poly_ordered;
 }
 
-int main () {
+int main()
+{
 
-  List *pA = create ();
-  pA = insert (pA, (poly){2, 0});
-  pA = insert (pA, (poly){4, 1});
-  pA = insert (pA, (poly){5, 2});
-  pA = insert (pA, (poly){7, 5});
-  print (pA);
+  List *pA = create();
+  pA = insert(pA, (poly){2, 0});
+  pA = insert(pA, (poly){4, 1});
+  pA = insert(pA, (poly){5, 2});
+  pA = insert(pA, (poly){7, 5});
+  print(pA);
 
-  List *pB = create ();
-  pB = insert (pB, (poly){-5, 0});
-  pB = insert (pB, (poly){-4, 1});
-  pB = insert (pB, (poly){5, 2});
-  pB = insert (pB, (poly){3, 6});
-  print (pB);
+  List *pB = create();
+  pB = insert(pB, (poly){-5, 0});
+  pB = insert(pB, (poly){-4, 1});
+  pB = insert(pB, (poly){5, 2});
+  pB = insert(pB, (poly){3, 6});
+  print(pB);
 
-  List *pC = poly_sum (pA, pB);
-  print (pC);
+  List *pC = poly_sum(pA, pB);
+  print(pC);
 
-  destroy (pA);
-  destroy (pB);
-  destroy (pC);
+  destroy(pA);
+  destroy(pB);
+  destroy(pC);
 }
