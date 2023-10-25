f246bf0a160590e02ed53262557e89722280907e
Gustavo Santos <gefgu@hotmail.com>
2023-10-25 17:48:02 -0300

    aula10: fix streamlit

--- aula10/extra/app.py
+++ aula10/extra/app.py
@@ -9,11 +9,14 @@ st.text("Altos valores iniciais para x, y, 'box size' podem resultar em delay.")
 
 btn = st.button("Play")
 
-print(os.listdir("."))
-print(os.listdir("./app/static/"))
-image = cv2.imread("./app/static/lena.pgm")
-grayscale_image = cv2.imread("./app/static/lena.pgm", -1)
-final_image = np.zeros((grayscale_image.shape), dtype=np.uint8)
+try:
+  image = cv2.imread("./lena.pgm")
+  grayscale_image = cv2.imread("./lena.pgm", -1)
+  final_image = np.zeros((grayscale_image.shape), dtype=np.uint8)
+except:
+  image = cv2.imread("https://github.com/gefgu/ed1/blob/main/aula10/extra/lena.pgm")
+  grayscale_image = cv2.imread("https://github.com/gefgu/ed1/blob/main/aula10/extra/lena.pgm", -1)
+  final_image = np.zeros((grayscale_image.shape), dtype=np.uint8)
 
 box_size = st.slider("Box Size", 3, 25, 3, 2)
 
