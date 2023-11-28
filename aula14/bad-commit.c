beeddf9a3ac54fdf172b54f33023106d0124a01e
Gustavo Santos <gefgu@hotmail.com>
2023-11-27 21:35:25 -0300

    aula14: bad commit

--- aula14/bad-commit.c
+++ aula14/bad-commit.c
@@ -1,15 +1,31 @@
 #include "utils.h"
 
 /* */
-int bad_commit (int *A, int n) {
-  /*Terminar*/	
-} 
+int bad_commit(int *A, int n)
+{
+  /*Terminar*/
+  int l = 0;
+  int r = n - 1;
+  int m;
+
+  while (l <= r)
+  {
+    m = (l + r) / 2;
+    if (A[m] == 0)
+      l = m + 1;
+    else
+      r = m - 1;
+  }
+
+  return l;
+}
 
 /* */
-int main () {
+int main()
+{
   int i;
   int n = 7;
-  int A[] = {0,0,0,0,0,0,1};
+  int A[] = {0, 0, 0, 0, 0, 0, 1};
   printf("Bad commit: %d\n", bad_commit(A, n));
   return 0;
 }
